730a3dd127136e09f95e43b628ba432c83457746
sanjay12345 <58308698+JUGALMEHRA958@users.noreply.github.com>
2022-05-17 17:07:50 +0530

    added addtolast in linkedlist program in java

--- Java/Singlylinked.java
+++ Java/Singlylinked.java
@@ -15,8 +15,38 @@ public class Singlylinked {
 
         public Node(int data) {
             this.data = data;
+            this.next=null;
         }
     }
+
+    public void addAtFront(int data){
+        // System.out.println("Data is  " + data);
+        Node newNode = new Node(data);
+        newNode.next = head;
+        // System.out.println("Node data  is   " + newNode.data + "      head is  " + head.data);
+        head = newNode;
+    }
+    
+    public void addAtLast(int data){
+        // System.out.println("Data to add in last  is  " + data);
+        Node newNode = new Node(data);
+        Node temp = head;
+        if(head == null){
+            // System.out.println("Head is null");
+            head = newNode;
+            return;
+        }
+        else{
+            while(temp.next!= null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        
+        
+    }
+
+
 
     public void display() {
         Node temp = head;
@@ -45,17 +75,24 @@ public class Singlylinked {
         Node third = new Node(87);
         Node fourth = new Node(45);
         Node fifth = new Node(100);
+        
 
         sl.head.next = second;
         second.next = third;
         third.next = fourth;
         fourth.next = fifth;
         fifth.next = null;
-
+        sl.addAtFront(1330);
         sl.display();
+
+        
+        sl.addAtLast(1330);
+        sl.display();
+
+
         sl.findLength();
 
-        System.out.println("Over");
+        // System.out.println("Over");
         // System.out.println( "Hello World!" );
     }
 }
